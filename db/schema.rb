bc5e2139c58c5d8219b81589126343cfce47e514
vincent <vincent4502237@gmail.com>
2018-05-05 01:34:09 +0800

    use psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180504114947) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "wechat_users", force: :cascade do |t|
     t.string "open_id"
     t.string "nickname"
