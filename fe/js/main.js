e5090cd044dc662a68342d0f6960d7c71241f875
Bruce Li <bruce.li@strikingly.com>
2018-05-05 14:24:04 +0800

    feat(difficulty): increase difficulty by shooting fast

--- fe/js/main.js
+++ fe/js/main.js
@@ -19,6 +19,8 @@ let ctx = canvas.getContext('2d')
 let databus = new DataBus()
 let intervalID = null
 let appearredBadman = false
+// TODO: FIXME
+let badmanShootSpeed = 100
 
 /**
  * 游戏主函数
@@ -359,13 +361,20 @@ export default class Main {
     }
 
     // TODO: FIXME
-    if (databus.frame % 100 === 0 && this.badman) {
+    if (databus.frame % badmanShootSpeed === 0 && this.badman) {
       this.badman.shoot()
     }
 
     if (databus.score > 10) {
       this.setupBadman()
     }
+
+    this.updateBadmanShootSpeed()
+  }
+
+  updateBadmanShootSpeed() {
+    const maxBadmanShootSpeed = 30
+    badmanShootSpeed = Math.max(maxBadmanShootSpeed, 100 - databus.score * 2)
   }
 
   // 实现游戏帧循环
