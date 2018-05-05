dad83f9daed4aa82ac1ccbea209a04b42a251f08
vincent <vincent4502237@gmail.com>
2018-05-05 09:39:10 +0800

    update bg image

--- fe/js/runtime/background.js
+++ fe/js/runtime/background.js
@@ -3,7 +3,7 @@ import Sprite from '../base/sprite'
 const screenWidth  = window.innerWidth
 const screenHeight = window.innerHeight
 
-const BG_IMG_SRC   = 'images/bg.jpg'
+const BG_IMG_SRC   = 'images/bg.png'
 const BG_WIDTH     = 512
 const BG_HEIGHT    = 512
 
