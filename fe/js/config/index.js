40fe06c4471fa71f8611b77e80b236d1a9ef0287
Bruce Li <bruce.li@strikingly.com>
2018-05-06 14:11:34 +0800

    life to 3

--- fe/js/config/index.js
+++ fe/js/config/index.js
@@ -1,7 +1,7 @@
 // 子弹皮肤
 export const LOVE_BULLET_IMG_SRC = 'images/loveBullet.png'
 export const BAD_BULLET_IMG_SRC = 'images/badBullet.png'
-export const MAX_LIFE = 1
+export const MAX_LIFE = 3
 
 export const BADMAN_IMG_SRC = 'images/uncle.png'
 export const GODMAN_IMG_SRC = 'images/godman.png'
