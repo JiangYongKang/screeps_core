f160d587b888a005320889744a899f60cec79997
JiangYongKang <vincent4502237@gmail.com>
2020-09-27 15:31:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 module.exports.loop = function () {
 
     // 如果当前的 creeps 数量小于 10 个，并且核心中剩余的能量大于 300 则自动生成一个新的 creeps
-    if (Object.values(Game.creeps).filter(creep => creep.memory.role === 'WORKER').length < 3 && Game.spawns['spawn'].energy >= 300) {
-        let creepName = Math.random().toString(16).substring(2, 6).toUpperCase()
+    if (Object.values(Game.creeps).filter(creep => creep.memory.role === 'WORKER').length < 1 && Game.spawns['spawn'].energy >= 300) {
+        let creepName = 'W' + Math.random().toString(16).substring(2, 6).toUpperCase()
         Game.spawns['spawn'].spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {
             memory: {
                 role: 'WORKER',
@@ -13,7 +13,7 @@ module.exports.loop = function () {
 
     // 如果当前的 creeps 数量小于 10 个，并且核心中剩余的能量大于 300 则自动生成一个新的 creeps
     if (Object.values(Game.creeps).filter(creep => creep.memory.role === 'UPGRADER').length < 3 && Game.spawns['spawn'].energy >= 300) {
-        let creepName = Math.random().toString(16).substring(2, 6).toUpperCase()
+        let creepName = 'U' + Math.random().toString(16).substring(2, 6).toUpperCase()
         Game.spawns['spawn'].spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {
             memory: {
                 role: 'UPGRADER',
@@ -22,8 +22,9 @@ module.exports.loop = function () {
         })
     }
 
-    if (Object.values(Game.creeps).filter(creep => creep.memory.role === 'BUILDER').length < 3 && Game.spawns['spawn'].energy >= 300) {
-        let creepName = Math.random().toString(16).substring(2, 6).toUpperCase()
+    // 如果当前的 creeps 数量小于 10 个，并且核心中剩余的能量大于 300 则自动生成一个新的 creeps
+    if (Object.values(Game.creeps).filter(creep => creep.memory.role === 'BUILDER').length < 5 && Game.spawns['spawn'].energy >= 300) {
+        let creepName = 'B' + Math.random().toString(16).substring(2, 6).toUpperCase()
         Game.spawns['spawn'].spawnCreep([WORK, CARRY, MOVE, MOVE], creepName, {
             memory: {
                 role: 'BUILDER',
@@ -32,22 +33,18 @@ module.exports.loop = function () {
         })
     }
 
-    // 循环所有的 creeps
     Object.values(Game.creeps).filter(creep => creep.memory.role === 'WORKER').forEach(creep => {
-        // 如果 creep 身上的能源等于它可以负载的最大能源，则将 full 置为 true
         if (creep.carry.energy === creep.carryCapacity) {
             creep.memory.isEmpty = false
         }
         if (creep.carry.energy === 0) {
             creep.memory.isEmpty = true
         }
-        // 如果不是满载的情况，则移动到 spawn 去采矿
         if (creep.memory.isEmpty) {
             let sources = creep.room.find(FIND_SOURCES)
             if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE)
                 creep.moveTo(sources[1], { visualizePathStyle: { stroke: '#ffaa00' } })
         } else {
-            // 如果满载的情况，则去采集矿物并将矿物卸载到核心中
             if (creep.transfer(Game.spawns['spawn'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns['spawn'], { visualizePathStyle: { stroke: '#ffaa00' } })
             }
@@ -62,7 +59,6 @@ module.exports.loop = function () {
     })
 
     Object.values(Game.creeps).filter(creep => creep.memory.role === 'UPGRADER').forEach(creep => {
-        // 如果 creep 身上的能源等于它可以负载的最大能源，则将 full 置为 true
         if (creep.carry.energy === creep.carryCapacity) {
             creep.memory.isEmpty = false
         }
@@ -70,7 +66,7 @@ module.exports.loop = function () {
             creep.memory.isEmpty = true
         }
         if (creep.memory.isEmpty) {
-            let sources = creep.room.find(FIND_SOURCES);
+            let sources = creep.room.find(FIND_SOURCES)
             if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
             }
@@ -89,9 +85,9 @@ module.exports.loop = function () {
             creep.memory.isEmpty = true
         }
         if (creep.memory.isEmpty) {
-            let sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
+            let sources = creep.room.find(FIND_SOURCES)
+            if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], { visualizePathStyle: { stroke: '#ffaa00' } })
             }
         } else {
             let targets = creep.room.find(FIND_CONSTRUCTION_SITES)
